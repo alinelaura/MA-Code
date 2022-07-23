fb21bb8316f6024ebfd9ee3ed06499129fb3196c
alinelaura <54060205+alinelaura@users.noreply.github.com>
2022-07-23 10:48:28 +0200

    Prepare Swissvotes Data

--- Code/MA_VoxVotoPreparation.R
+++ Code/MA_VoxVotoPreparation.R
@@ -1,1 +1,23 @@
-################################Prepare Vox/Voto Data###########################
+##############################Prepare Vox/Voto Data##############################
+
+# Libraries
+library(tidyverse)
+library(lubridate)
+
+
+
+swissvotes <- read.csv("./Data/swissvotes/swissvotes.csv", sep = ';')
+labels <- read.csv("./Data/swissvotes/vote_labels.csv", sep = ';') %>% 
+  mutate(Code = as.character(Code))
+
+# Prepare Data for Analysis
+swissvotes <- swissvotes %>% 
+  mutate(across(starts_with('dat'), ~ as.Date(as.character(.), format = '%d%m%Y'))) %>% 
+  mutate(across(starts_with('d1e'), ~ as.character(.))) %>% 
+  left_join(labels, by = c("d1e1" = "Code")) %>% 
+  dplyr::rename(d1e1_label = Label) %>% 
+  left_join(labels, by = c("d1e2" = "Code")) %>% 
+  dplyr::rename(d1e2_label = Label) %>% 
+  left_join(labels, by = c("d1e3" = "Code")) %>% 
+  dplyr::rename(d1e3_label = Label)
+
